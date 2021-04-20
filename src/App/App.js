549cf43943b29ff11bcbcf9f840103a943dfb5ec
Wes <mwg1825@aol.com>
2021-04-19 20:59:30 -0600

    add browser router

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import { Route } from "react-router-dom";
+import LandingPage from "../LandingPage/LandingPage";
+
+function App() {
+  return (
+    <main className="App">
+      <Route exact path="/" component={LandingPage} />
+    </main>
+  );
+}
+
+export default App;
