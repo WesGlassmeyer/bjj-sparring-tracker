543bd339bf966a57202d7966aff42f687659a547
Wes <mwg1825@aol.com>
2021-04-20 21:08:25 -0600

    Update LandingPage.js
    remove email field,
    add demo button

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -10,11 +10,16 @@ export default function LandingPage() {
           <h1>Sign Up</h1>
           <p>Please fill in this form to create an account.</p>
 
-          <label for="email">
-            <b>Email</b>
+          <label for="username">
+            <b>Username</b>
           </label>
-          <input type="text" placeholder="Enter Email" name="email" required />
-
+          <input
+            type="username"
+            placeholder="Username"
+            name="username"
+            required
+          />
+          <br />
           <label for="psw">
             <b>Password</b>
           </label>
@@ -25,30 +30,29 @@ export default function LandingPage() {
             required
           />
 
-          <label for="username">
-            <b>Username</b>
-          </label>
-          <input
-            type="username"
-            placeholder="Username"
-            name="username"
-            required
-          />
           <div class="signup">
             <button type="submit" class="signupbtn">
               Sign Up
             </button>
           </div>
-
-          <p>
-            Have an account? <Link to="/login">Login</Link>
-          </p>
+          <div class="login">
+            <p>
+              Have an account? <br />
+              <button type="submit" class="loginbtn">
+                <Link to="/sparring_log">Login</Link>
+              </button>
+            </p>
+          </div>
+          <div class="demo">
+            <p>
+              Check it out using a demo account! <br />
+              <button type="submit" class="demobtn">
+                Demo
+              </button>
+            </p>
+          </div>
         </div>
       </form>
     </div>
   );
-}
-
-{
-  /* <a href="#">Login</a>. */
 }
