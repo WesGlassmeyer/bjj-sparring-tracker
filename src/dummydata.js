04118853939d069a9d3ee81710336e5184cfbace
Wes <mwg1825@aol.com>
2021-04-20 21:07:26 -0600

    Create dummydata.js

---
+++ src/dummydata.js
@@ -0,0 +1,78 @@
+const dummydata = {
+  entries: [
+    {
+      date: "April 18, 2021",
+      cardio: {
+        rounds: 1,
+        roundLength: 15,
+        rating: 5,
+        totalLength: 15,
+      },
+      submissions: {
+        count: 1,
+        category: "chokes",
+        subName: "Rear Naked Choke",
+      },
+      taps: {
+        count: 2,
+        category: "arm attacks",
+        subName: "Arm Bar",
+      },
+      sweeps: {
+        count: 3,
+        category: "open guard",
+        sweepName: "Sumi Gaeshi",
+      },
+    },
+    {
+      date: "April 19, 2021",
+      cardio: {
+        rounds: 2,
+        roundLength: 10,
+        rating: 4,
+        totalLength: 20,
+      },
+      submissions: {
+        count: 2,
+        category: "leg attacks",
+        subName: "Ankle Lock",
+      },
+      taps: {
+        count: 1,
+        category: "chokes",
+        subName: "Arm Triangle",
+      },
+      sweeps: {
+        count: 3,
+        category: "closed guard",
+        sweepName: "Pendulum Sweep",
+      },
+    },
+    {
+      date: "April 20, 2021",
+      cardio: {
+        rounds: 3,
+        roundLength: 5,
+        rating: 3,
+        totalLength: 15,
+      },
+      submissions: {
+        count: 4,
+        category: "arm attacks",
+        subName: "kimura",
+      },
+      taps: {
+        count: 2,
+        category: "leg attacks",
+        subName: "Heel Hook",
+      },
+      sweeps: {
+        count: 2,
+        category: "closed guard",
+        sweepName: "Hip Bump",
+      },
+    },
+  ],
+};
+
+export default dummydata;
