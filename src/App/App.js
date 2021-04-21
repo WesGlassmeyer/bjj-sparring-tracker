ac7d9e6c65c067a0f14bb38a951ed4a824c90b9e
Wes <mwg1825@aol.com>
2021-04-20 21:07:21 -0600

    add log page route

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 // import './App.css';
 import { Route } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
+import LogPage from "../LogPage/LogPage";
 
 function App() {
   return (
     <main className="App">
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/sparring_log" component={LogPage} />
     </main>
   );
 }
