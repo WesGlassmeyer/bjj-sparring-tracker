e6ccd729f1b4596892146a625dce169a2960b621
Wes <mwg1825@aol.com>
2021-04-20 21:09:13 -0600

    create log list

---
+++ src/LogList/LogList.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import LogEntry from "../LogEntry/LogEntry";
+import "./LogList.css";
+// import config from "../config";
+
+class LogList extends Component {
+  state = {
+    entries: [],
+    error: null,
+  };
+
+  //   setEntries = (entries) => {
+  //     this.setState({
+  //       entries,
+  //       error: null,
+  //     });
+  //   };
+
+  //   componentDidMount() {
+  //     fetch(config.SERVER_endpoint)
+  //       .then((res) => {
+  //         if (!res.ok) {
+  //           return res.json().then((error) => Promise.reject(error));
+  //         }
+
+  //         return res.json();
+  //       })
+  //       .then((res) => this.setEntries(res))
+  //       .catch((error) => {
+  //         console.error(error);
+  //         this.setState({ error });
+  //       });
+  //   }
+
+  render() {
+    const logEntries = this.state.entries;
+    if (this.state.entries.length === 0) {
+      return <section className="LogEntryListLoad">Loading...</section>;
+    }
+
+    return (
+      <section className="LogList">
+        <ul className="LogList__list" aria-live="polite">
+          {logEntries.map((entry) => (
+            <LogEntry key={entry.id} entry={entry} />
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default LogList;
