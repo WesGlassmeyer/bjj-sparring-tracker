e78dd300c0bd791d6196241b0bfe46f51eaeec99
Wes <mwg1825@aol.com>
2021-04-22 17:19:30 -0600

    add specs page route

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 import LogPage from "../LogPage/LogPage";
 import DetailPage from "../DetailPage/DetailPage";
+import SpecPage from "../SpecPage/SpecPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/sparring_log" component={LogPage} />
       <Route exact path="/sparring_details" component={DetailPage} />
+      <Route exact path="/sparring_specs" component={SpecPage} />
     </main>
   );
 }
