2e92aef8079bb7b6e83eb82284347042cb27fdbe
Wes <mwg1825@aol.com>
2021-04-21 20:19:26 -0600

    add link to detail page

--- src/LogPage/LogPage.js
+++ src/LogPage/LogPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import LogList from "../LogList/LogList";
+import { Link } from "react-router-dom";
 import "./LogPage.css";
 
 export default class LogPage extends Component {
@@ -9,6 +10,11 @@ export default class LogPage extends Component {
         <h2 className="log-page-title">Sparring Log</h2>
 
         <LogList />
+        <div class="add-log">
+          <button type="submit" class="add-logbtn">
+            <Link to="/sparring_details">Add Sparring Log</Link>
+          </button>
+        </div>
       </div>
     );
   }
