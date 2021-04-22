4ec4a6ac10046f3a9a95ba5bb75bb5881a270cf4
Wes <mwg1825@aol.com>
2021-04-21 20:15:29 -0600

    add sparring details route

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@
 import { Route } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 import LogPage from "../LogPage/LogPage";
+import DetailPage from "../DetailPage/DetailPage";
 
 function App() {
   return (
     <main className="App">
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/sparring_log" component={LogPage} />
+      <Route exact path="/sparring_details" component={DetailPage} />
     </main>
   );
 }
