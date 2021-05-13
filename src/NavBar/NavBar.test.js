2bade4f7bab2ff88593649a2c9a178b7604573f0
Wes <mwg1825@aol.com>
2021-05-12 19:54:05 -0600

    Update NavBar.test.js

--- src/NavBar/NavBar.test.js
+++ src/NavBar/NavBar.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./Navbar";
+import NavBar from "./NavBar";
 import { BrowserRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <BrowserRouter>
-      <Navbar />
+      <NavBar />
     </BrowserRouter>,
     div
   );
