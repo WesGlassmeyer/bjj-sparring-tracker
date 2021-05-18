c0f2906544b1b4b5b5ec0ce0a7f25827acd77013
Wes <mwg1825@aol.com>
2021-05-17 20:27:50 -0600

    Update SpecPage.test.js

--- src/SpecPage/SpecPage.test.js
+++ src/SpecPage/SpecPage.test.js
@@ -4,12 +4,17 @@ import SpecPage from "./SpecPage";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  const detailPageState = () => {};
+  const detailPageState = () => {
+    return [
+      { name: "Arm Bar", count: 2 },
+      { name: "Triangle", count: 4 },
+    ];
+  };
   const setSpecPageState = () => {};
 
   ReactDOM.render(
     <SpecPage
-      title={"Submissons"}
+      title={"Submissions"}
       setSpecPageState={setSpecPageState}
       //   changeForms={this.changeForms}
       detailPageState={detailPageState}
