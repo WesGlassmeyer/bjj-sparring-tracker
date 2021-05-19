a75e0977d56a331c18130db25ea4288384dfed66
Wes <mwg1825@aol.com>
2021-05-18 18:35:58 -0600

    update choke options

--- src/SpecPage/SpecPage.js
+++ src/SpecPage/SpecPage.js
@@ -75,7 +75,7 @@ export default class SpecPage extends Component {
       chokes: {
         id: "chokes",
         label: "Select Submissions",
-        options: ["Rear Naked", "Triangle", "Arm Triangle", "Bow and Arrow"],
+        options: ["Rear Naked", "Triangle", "Head and Arm", "Collar Choke"],
       },
       leg_attacks: {
         id: "leg attacks",
