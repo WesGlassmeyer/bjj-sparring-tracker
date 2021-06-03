5aaf0088b68f2f181d76ce9e49adf005fcc5c275
Wes <mwg1825@aol.com>
2021-06-02 20:43:40 -0600

    fetch from api

--- src/LogList/LogList.js
+++ src/LogList/LogList.js
@@ -1,45 +1,44 @@
 import React, { Component } from "react";
 import LogEntry from "../LogEntry/LogEntry";
 import "./LogList.css";
-import dummydata from "../dummydata";
-// import config from "../config";
+import config from "../config";
 
 export default class LogList extends Component {
-  // state = {
-  //   entries: [],
-  //   error: null,
-  // };
+  state = {
+    entries: [],
+    error: null,
+    isLoading: true,
+  };
 
-  //   setEntries = (entries) => {
-  //     this.setState({
-  //       entries,
-  //       error: null,
-  //     });
-  //   };
+  setEntries = (entries) => {
+    this.setState({
+      entries,
+      error: null,
+      isLoading: false,
+    });
+  };
 
-  //   componentDidMount() {
-  //     fetch(config.SERVER_endpoint)
-  //       .then((res) => {
-  //         if (!res.ok) {
-  //           return res.json().then((error) => Promise.reject(error));
-  //         }
-
-  //         return res.json();
-  //       })
-  //       .then((res) => this.setEntries(res))
-  //       .catch((error) => {
-  //         console.error(error);
-  //         this.setState({ error });
-  //       });
-  //   }
+  componentDidMount() {
+    fetch(config.SERVER_endpoint)
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => Promise.reject(error));
+        }
+        return res.json();
+      })
+      .then((res) => this.setEntries(res))
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error });
+      });
+  }
 
   render() {
-    // const logEntries = this.state.entries;
-    // if (this.state.entries.length === 0) {
-    //   return <section className="LogEntryListLoad">Loading...</section>;
-    // }
+    const logEntries = this.state.entries;
+    if (this.state.isLoading) {
+      return <section className="LogEntryListLoad">Loading...</section>;
+    }
 
-    const logEntries = dummydata.entries;
     return (
       <section className="log-list">
         <ul className="log-list__list" aria-live="polite">
