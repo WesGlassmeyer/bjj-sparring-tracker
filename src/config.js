4049d6fb4cbd55b23c817bd67b928e887c7852fa
Wes <mwg1825@aol.com>
2021-06-02 20:41:57 -0600

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  SERVER_endpoint:
+    //   process.env.REACT_APP_SERVER_ENDPOINT || "http://localhost:8000/log_entry",
+    "http://localhost:8000/log_entry",
+};
+
+export default config;
