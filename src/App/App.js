51668a31ded6773e298bd59e9d7288d45f1b5895
Wes <mwg1825@aol.com>
2021-06-11 22:09:20 -0600

    move navbar from main element

--- src/App/App.js
+++ src/App/App.js
@@ -3,13 +3,11 @@ import { Route } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 import LogPage from "../LogPage/LogPage";
 import StatsPage from "../StatsPage/StatsPage";
-import NavBar from "../NavBar/NavBar";
 
 export default class App extends Component {
   render() {
     return (
       <main className="App">
-        <NavBar></NavBar>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/sparring_log" component={LogPage} />
         <Route exact path="/sparring_stats" component={StatsPage} />
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App/App";
+import NavBar from "./NavBar/NavBar";
 
 ReactDOM.render(
   <BrowserRouter>
+    <NavBar />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
