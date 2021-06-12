18310ef3bd4c7edc692db192757191f3c0b45f72
Wes <mwg1825@aol.com>
2021-06-11 22:41:22 -0600

    move nav out of main

--- src/App/App.js
+++ src/App/App.js
@@ -3,15 +3,21 @@ import { Route } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 import LogPage from "../LogPage/LogPage";
 import StatsPage from "../StatsPage/StatsPage";
+import NavBar from "../NavBar/NavBar";
 
 export default class App extends Component {
   render() {
     return (
-      <main className="App">
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/sparring_log" component={LogPage} />
-        <Route exact path="/sparring_stats" component={StatsPage} />
-      </main>
+      <>
+        <header>
+          <NavBar></NavBar>
+        </header>
+        <main className="App">
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/sparring_log" component={LogPage} />
+          <Route exact path="/sparring_stats" component={StatsPage} />
+        </main>
+      </>
     );
   }
 }
