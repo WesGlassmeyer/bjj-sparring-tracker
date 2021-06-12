4fe0f34a045d3792fef239db553b37a4ce63e6f6
Wes <mwg1825@aol.com>
2021-06-11 22:41:54 -0600

    remove nav

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App/App";
-import NavBar from "./NavBar/NavBar";
 
 ReactDOM.render(
   <BrowserRouter>
-    <NavBar />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
